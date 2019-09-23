936f70c5579e7f4e7488b887e60abed6e02aac38
xy348735514 <348735514@163.com>
2019-09-23 21:25:52 +0800

    提交ndk-ncnn

---
+++ README.md
@@ -0,0 +1,17 @@
+# squeezencnn
+squeezenet ncnn example for android
+
+Both use Android.mk or CMakeLists.txt to compile share lib are working!
+
+## Usage
+1. download or compile libncnn.so by yourself from https://github.com/tencent/ncnn.
+   (just compatible with release 20181228, not support Vulkan now.)
+2. modify Android.mk or CMakeLists.txt and index NCNN_PATH to your ncnn library.
+3. compile project with your Android Studio.
+
+## Credit
+https://github.com/tencent/ncnn
+
+### License
+BSD 3 Clause
+
---
+++ app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/squeezencnn/squeezencnn_jni.o.d
@@ -0,0 +1,176 @@
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs-debug/squeezencnn/squeezencnn_jni.o: \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/squeezencnn_jni.cpp \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/string \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__config \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/string_view \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__string \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/algorithm \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/initializer_list \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstddef \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/version \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/type_traits \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstring \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/string.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/utility \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__tuple \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstdint \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/stdint.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__debug \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/memory \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/typeinfo \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/exception \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstdlib \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/new \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/limits \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/iterator \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/iosfwd \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/wchar.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__functional_base \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/tuple \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/stdexcept \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cassert \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/atomic \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/functional \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/bit \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstdio \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cwchar \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cwctype \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cctype \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/ctype.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/wctype.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/vector \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__bit_reference \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/climits \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/limits.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__split_buffer \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/net.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/platform.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/blob.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/layer.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/math.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/mat.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/allocator.h \
+  /Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/list \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/gpu.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/modelbin.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/option.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/paramdict.h \
+  /Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/squeezenet_v1.1.id.h
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/string:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__config:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/string_view:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__string:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/algorithm:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstddef:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/version:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/type_traits:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstring:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/utility:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__tuple:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstdint:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__debug:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/memory:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/exception:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/new:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/limits:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/iterator:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/tuple:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cassert:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/atomic:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/functional:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/bit:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cstdio:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cwchar:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cwctype:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/cctype:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/wctype.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/vector:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__bit_reference:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/climits:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/limits.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/__split_buffer:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/net.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/platform.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/blob.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/layer.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/math.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/mat.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/allocator.h:
+
+/Users/user/Library/Android/sdk/ndk-bundle1/sources/cxx-stl/llvm-libc++/include/list:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/gpu.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/modelbin.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/option.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/ncnn-android-lib/include/paramdict.h:
+
+/Users/user/Documents/demo201908-ndk/squeezencnn-master/app/src/main/jni/squeezenet_v1.1.id.h:
